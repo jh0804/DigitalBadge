479c18dbf408d8938bac8a270ce721ff2672383a
JH <wngml0708@gmail.com>
2023-09-25 12:41:22 +0900

    spring 프로젝트 생성

---
+++ backEnd/src/test/java/com/bookBadge/backEnd/BlockChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookBadge.backEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
