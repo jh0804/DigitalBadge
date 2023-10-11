da022a9e7b20147fb5722a5531d172f17fc490d3
JH <wngml0708@gmail.com>
2023-10-11 14:08:54 +0900

    로그인 구현 중2

---
+++ backEnd/src/main/java/com/bookbadge/backend/config/auth/SessionUser.java
@@ -0,0 +1,20 @@
+package com.bookbadge.backend.config.auth;
+
+import java.io.Serializable;
+
+import com.bookbadge.backend.user.User;
+
+import lombok.Getter;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user){
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
