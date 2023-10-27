c688b678e721b86c06f53b5813f26012cb57b8ec
CodenameUS <cws1344@naver.com>
2023-10-27 16:19:28 +0900

    badge.go, user.go 내용 추가

--- blockchain/chaincode/badge.go
+++ blockchain/chaincode/badge.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
@@ -20,6 +21,7 @@ type User struct {
 	ID        string   `json:"id"`
 	Name      string   `json:"name"`
 	BadgeIDs  []string `json:"badgeIds"`
+	Count     int      `json:"count"`  // Count 필드 추가
 }
 
 type BadgeContract struct {
@@ -31,9 +33,24 @@ func (c *BadgeContract) Init(ctx contractapi.TransactionContextInterface) error 
 	return nil
 }
 
-// 배지 발급
+func (c *BadgeContract) Invoke(ctx contractapi.TransactionContextInterface) error {
+	function, args := ctx.GetStub().GetFunctionAndParameters()
+
+	if function == "IssueBadge" {
+		return c.IssueBadge(ctx, args[0], args[1], args[2], args[3])
+	} else if function == "GetUserBadges" {
+		return c.GetUserBadges(ctx, args[0])
+	} else if function == "UpdateUserCount" {
+		return c.UpdateUserCount(ctx, args[0], args[1])
+	} else if function == "GetBadge" {
+		return c.GetBadge(ctx, args[0])
+	}
+
+	return fmt.Errorf("Invalid function name: %s", function)
+}
+
+// 배지 발급 함수
 func (c *BadgeContract) IssueBadge(ctx contractapi.TransactionContextInterface, badgeID, issuer, description, ownerID string) error {
-	// Check if the client has the 'admin' role
 	isAdmin, err := c.checkUserRole(ctx, "admin")
 	if err != nil {
 		return err
@@ -42,52 +59,58 @@ func (c *BadgeContract) IssueBadge(ctx contractapi.TransactionContextInterface, 
 		return errors.New("Only admins can issue badges")
 	}
 
-	badge := Badge{
-		ID:          badgeID,
-		Issuer:      issuer,
-		Description: description,
-		Owner:       ownerID,
-	}
-
-	badgeJSON, err := json.Marshal(badge)
-	if err != nil {
-		return err
-	}
-
-	err = ctx.GetStub().PutState(badgeID, badgeJSON)
-	if err != nil {
-		return err
-	}
-
-	// Add badgeID to the user's badge list
 	userKey := "USER_" + ownerID
 	userJSON, err := ctx.GetStub().GetState(userKey)
 	if err != nil {
 		return err
 	}
+	if userJSON == nil {
+		return fmt.Errorf("User with ID %s does not exist", ownerID)
+	}
 
 	var user User
-	if userJSON == nil {
-		user = User{
-			ID:        ownerID,
-			Name:      "Sample User", // 사용자의 이름 또는 다른 정보를 추가할 수 있습니다.
-			BadgeIDs:  []string{badgeID},
+	err = json.Unmarshal(userJSON, &user)
+	if err != nil {
+		return err
+	}
+
+	// Count가 5의 배수일 때 새로운 배지 발급
+	if user.Count%5 == 0 && user.Count != 0 {
+		badge := Badge{
+			ID:          badgeID,
+			Issuer:      issuer,
+			Description: description,
+			Owner:       ownerID,
 		}
-	} else {
-		err = json.Unmarshal(userJSON, &user)
+
+		badgeJSON, err := json.Marshal(badge)
+		if err != nil {
+			return err
+		}
+
+		err = ctx.GetStub().PutState(badgeID, badgeJSON)
 		if err != nil {
 			return err
 		}
 
 		user.BadgeIDs = append(user.BadgeIDs, badgeID)
+
+		// Count를 증가시킴
+		user.Count++
+		userJSON, err = json.Marshal(user)
+		if err != nil {
+			return err
+		}
+
+		err = ctx.GetStub().PutState(userKey, userJSON)
+		if err != nil {
+			return err
+		}
+
+		return nil
 	}
 
-	userJSON, err = json.Marshal(user)
-	if err != nil {
-		return err
-	}
-
-	return ctx.GetStub().PutState(userKey, userJSON)
+	return fmt.Errorf("User's count (%d) is not a multiple of 5", user.Count)
 }
 
 // 사용자 배지 목록 조회
@@ -126,6 +149,51 @@ func (c *BadgeContract) GetUserBadges(ctx contractapi.TransactionContextInterfac
 	}
 
 	return badges, nil
+}
+
+// 사용자의 Count 정보 업데이트 함수 추가
+func (c *BadgeContract) UpdateUserCount(ctx contractapi.TransactionContextInterface, userID string, countStr string) error {
+	isAdmin, err := c.checkUserRole(ctx, "admin")
+	if err != nil {
+		return err
+	}
+	if !isAdmin {
+		return errors.New("Only admins can update user count")
+	}
+
+	count, err := strconv.Atoi(countStr)
+	if err != nil {
+		return errors.New("Invalid Count. It should be an integer.")
+	}
+
+	userKey := "USER_" + userID
+	userJSON, err := ctx.GetStub().GetState(userKey)
+	if err != nil {
+		return err
+	}
+	if userJSON == nil {
+		return fmt.Errorf("User with ID %s does not exist", userID)
+	}
+
+	var user User
+	err = json.Unmarshal(userJSON, &user)
+	if err != nil {
+		return err
+	}
+
+	// Count 정보 업데이트
+	user.Count = count
+	userJSON, err = json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	err = ctx.GetStub().PutState(userKey, userJSON)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // 사용자의 배지 조회
