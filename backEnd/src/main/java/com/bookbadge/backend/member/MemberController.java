c59158ef07c622e12633c1560c670e7bec5a1ddd
JH <wngml0708@gmail.com>
2023-11-17 07:07:46 +0900

    createBogo test2

--- backEnd/src/main/java/com/bookbadge/backend/member/MemberController.java
+++ backEnd/src/main/java/com/bookbadge/backend/member/MemberController.java
@@ -3,7 +3,7 @@ package com.bookbadge.backend.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
